40e60b29437355ef41ef0a85a4c588a831e9c3bf
Pak_Man926 <davidkamau925@gmail.com>
2025-01-24 01:39:32 +0300

    Third Commit

--- app/src/main/java/user/app/login/Login.java
+++ app/src/main/java/user/app/login/Login.java
@@ -1,10 +1,13 @@
 package user.app.login;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity
 {
+    private EditText txtUsername, txtPassword2;
+
     @Override
     public void onCreate(Bundle savedInstance)
     {
